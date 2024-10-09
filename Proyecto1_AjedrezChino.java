b19af060b936e31764354b30f851d494e9b42f0d
RoyGeova07 <royumana3@gmail.com>
2024-10-09 14:57:54 -0600

    Primer avance Dia 1

---
+++ Proyecto1_AjedrezChino.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto1_ajedrezchino;
+
+/**
+ *
+ * @author royum
+ */
+public class Proyecto1_AjedrezChino {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        // llamado al main 
+        
+        
+    }
+
+}
---
+++ User.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto1_ajedrezchino;
+
+/**
+ *
+ * @author royum
+ */
+public class User {
+    
+    private String nombre;
+    private String contrasena;
+    private int puntos;
+    private boolean Activo;
+    
+}
